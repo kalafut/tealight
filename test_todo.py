7869f940f70fa09337cb217b0b6984837ecb8776
Jim Kalafut <jim@kalafut.net>
2016-07-13 21:48:45 -0700

    Fix tests

--- test_todo.py
+++ test_todo.py
@@ -14,27 +14,15 @@ def test_parse():
     assert todo.completed is None
     assert todo.priority == 50
 
-    #assert todo.parse_line('2013-05-05  This is a test ') == {
-    #        'text': 'This is a test',
-    #        'done': False,
-    #        'created': test_date_1,
-    #        'completed': None,
-    #        'priority': 50,
-    #        }
-    #assert todo.parse_line('(A) This is a test ') == {
-    #        'text': 'This is a test',
-    #        'done': False,
-    #        'created': today,
-    #        'completed': None,
-    #        'priority': 90,
-    #        }
-    #assert todo.parse_line('x (B)   This is a test ').priority == 80
-    #assert todo.parse_line('x (C)   This is a test ').priority == 70
-    #assert todo.parse_line('x 2013-05-05   2016-12-03  This is a test ') == {
-    #        'text': 'This is a test',
-    #        'done': True,
-    #        'created': test_date_1,
-    #        'completed': test_date_2,
-    #        'priority': 50,
-    #        }
+    todo = Todo('(A) This is a test ')
+    assert todo.priority == 90
+    assert todo.created == today
+
+    assert Todo('x (B)   This is a test ').priority == 80
+    assert Todo('x (C)   This is a test ').priority == 70
+
+    todo = Todo('x 2013-05-05   2016-12-03  This is a test ')
+    assert todo.done is True
+    assert todo.created == test_date_1
+    assert todo.completed == test_date_2
 
